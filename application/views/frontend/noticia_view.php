a0982f425fc2262029d97662fd802ebb20b57bbd
Hugo Montes <hugomontes927@gmail.com>
2017-07-15 10:44:36 -0400

    Cambios Noticia

--- application/views/frontend/noticia_view.php
+++ application/views/frontend/noticia_view.php
@@ -77,9 +77,6 @@
 
   <!--[if IE 8]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-  <style type="text/css">
-
-  </style>
 </head>
 
 <body>
